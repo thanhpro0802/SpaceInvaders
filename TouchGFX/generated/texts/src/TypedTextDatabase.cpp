5f3920a31e8b1a1f917353c4258827c9cd361f9b
AnhTuanXD <anhtuan2232005@gmail.com>
2026-07-13 23:03:44 +0700

    Add retro start and game over UI

--- TouchGFX/generated/texts/src/TypedTextDatabase.cpp
+++ TouchGFX/generated/texts/src/TypedTextDatabase.cpp
@@ -5,14 +5,14 @@
 #include <fonts/GeneratedFont.hpp>
 #include <texts/TypedTextDatabase.hpp>
 
-extern touchgfx::GeneratedFont& getFont_verdana_20_4bpp();
-extern touchgfx::GeneratedFont& getFont_verdana_40_4bpp();
-extern touchgfx::GeneratedFont& getFont_verdana_10_4bpp();
+extern touchgfx::GeneratedFont& getFont_RetroGaming_20_4bpp();
+extern touchgfx::GeneratedFont& getFont_RetroGaming_40_4bpp();
+extern touchgfx::GeneratedFont& getFont_RetroGaming_10_4bpp();
 
 const touchgfx::Font* touchgfx_fonts[] = {
-    &(getFont_verdana_20_4bpp()),
-    &(getFont_verdana_40_4bpp()),
-    &(getFont_verdana_10_4bpp())
+    &(getFont_RetroGaming_20_4bpp()),
+    &(getFont_RetroGaming_40_4bpp()),
+    &(getFont_RetroGaming_10_4bpp())
 };
 
 extern const touchgfx::TypedText::TypedTextData typedText_database_DEFAULT[];
@@ -60,13 +60,13 @@ void resetFont(touchgfx::FontId fontId)
     switch (fontId)
     {
     case 0:
-        touchgfx_fonts[0] = &(getFont_verdana_20_4bpp());
+        touchgfx_fonts[0] = &(getFont_RetroGaming_20_4bpp());
         break;
     case 1:
-        touchgfx_fonts[1] = &(getFont_verdana_40_4bpp());
+        touchgfx_fonts[1] = &(getFont_RetroGaming_40_4bpp());
         break;
     case 2:
-        touchgfx_fonts[2] = &(getFont_verdana_10_4bpp());
+        touchgfx_fonts[2] = &(getFont_RetroGaming_10_4bpp());
         break;
     }
 }
